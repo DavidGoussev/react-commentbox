d60ba3d0f70ee41bb2d43c65c3bd8edc28fdf0f8
dpg5000 <dgoussev@gmail.com>
2015-09-26 12:24:34 -0700

    add list and form components

--- public/scripts/commentbox.js
+++ public/scripts/commentbox.js
@@ -2,11 +2,35 @@ var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-        Hello world!  I am a nice commentbox.
+        <h1>comments</h1>
+        <CommentList />
+        <CommentForm />
       </div>
     );
   }
 });
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello world, I am a comment list!
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world, I am a Comment Form!
+      </div>
+    );
+  }
+});
+
+
 React.render(
   <CommentBox />,
   document.getElementById('content')
